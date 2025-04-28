df0223838e068526c96ba77da77eb15d659a5e2d
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-28 17:12:51 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile{
+    public static void main(String[] args){
+        int x = 10;
+        do{
+            System.out.print("Value of x:"+x);
+            x++;
+            System.out.print("\n");
+        }while(x<20);
+    }
+}

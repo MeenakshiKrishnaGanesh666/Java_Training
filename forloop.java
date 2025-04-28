426a464fb971f18df0d13f154cfefa82dff7c793
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-28 14:37:20 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,12 @@
+public class forloop{
+    public static void main(String[] args) {
+        for(int i=10;i>1;i--){
+            System.out.print("The value of i is:"+i);
+            System.out.print("\n");
+        }
+        for(int i=1;i<11;i++){
+            System.out.print("The value of i is:"+i);
+            System.out.print("\n");
+        }
+    }
+}

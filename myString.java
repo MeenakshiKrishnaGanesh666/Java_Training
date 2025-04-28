f23f0afd46eed2bed6f08645364a9334138a1686
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-28 12:39:10 +0530

    Add files via upload

---
+++ myString.java
@@ -0,0 +1,14 @@
+public class myString{
+    public static void main(String[] args){
+        int myInt = 7;
+        String text = "Java";
+        String blank = " ";
+        String name = "John";
+        String greeting = text+blank+name;
+        System.out.print(greeting);
+        System.out.print("Java"+" "+"John");
+        System.out.print("My integer is:"+myInt);
+        double myDouble = 5.5;
+        System.out.print("My number is:"+myDouble+".");
+    }
+}

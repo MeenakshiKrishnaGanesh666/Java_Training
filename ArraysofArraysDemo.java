9d6ade40714301422c0bad425cc4c74e94d01116
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-29 11:19:57 +0530

    Add files via upload

--- ArraysofArraysDemo.java
+++ ArraysofArraysDemo.java
@@ -8,10 +8,11 @@ public class ArraysofArraysDemo {
             {"ScoobyDooGang", "ScoobyDoo", "Shaggy"}
         };
         for(int i=0; i<cartoons.length; i++){
-            System.out.print(cartoons[i][0]+":");
+            System.out.print(cartoons[i][0]+" ");
         for(int j=1; j<cartoons[i].length; j++){
-            System.out.print(cartoons[i][j]+":");
+            System.out.print(cartoons[i][j]+" ");
         }
+        System.out.print("\n");
     }
 }
 }

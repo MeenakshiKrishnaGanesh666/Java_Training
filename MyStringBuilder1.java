ea25bc34fb75ebe3dbac91dfa3daff14b0bf2268
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-29 16:01:58 +0530

    Add files via upload

---
+++ MyStringBuilder1.java
@@ -0,0 +1,9 @@
+class MyStringBuilder1{
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder(" ");
+        sb.append("My name is Chris");
+        sb.append(" ");
+        sb.append("I am a web developer");
+        System.out.println(sb.toString());
+    }
+}

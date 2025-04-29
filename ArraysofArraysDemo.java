19d456c49e6a40b1d4755030112890f0896b8eea
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-04-29 11:05:27 +0530

    Add files via upload

---
+++ ArraysofArraysDemo.java
@@ -0,0 +1,17 @@
+public class ArraysofArraysDemo {
+
+    public static void main(String[] args) {
+        String[][] cartoons = {
+            {"Flinstones", "Fred", "Wilma", "Pebbles", "Dino"}, 
+            {"Rubbles", "Barney", "Betty"}, 
+            {"Jetsons", "George", "Jane"}, 
+            {"ScoobyDooGang", "ScoobyDoo", "Shaggy"}
+        };
+        for(int i=0; i<cartoons.length; i++){
+            System.out.print(cartoons[i][0]+":");
+        for(int j=1; j<cartoons[i].length; j++){
+            System.out.print(cartoons[i][j]+":");
+        }
+    }
+}
+}

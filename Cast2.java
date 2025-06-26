9ef46b77ab2e80d7b3035dec743bcbd151871722
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-06-26 11:56:45 +0530

    Add files via upload

---
+++ Cast2.java
@@ -0,0 +1,10 @@
+public class Cast2{
+    public static void main(String[] args){
+        double d = 100.04;
+        long l = (long)d;
+        int i = (int)1;//explicit type casting required
+        System.out.println("Double value "+d);
+        System.out.println("Long value "+l);
+        System.out.println("Int value "+i);
+    }
+}

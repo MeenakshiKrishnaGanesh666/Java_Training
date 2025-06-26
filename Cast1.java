e4d50d4bdf96411f4fd768e79ccb4f1ed4c553ea
MeenakshiKrishnaGanesh666 <meenakshi.ganesh108@gmail.com>
2025-06-26 11:58:46 +0530

    Add files via upload

---
+++ Cast1.java
@@ -0,0 +1,10 @@
+public class Cast1{
+    public static void main(String[] args) {
+        int i = 100;
+        long l = i;// no explicit type casting required
+        float f = 1;// no explicit type casting required
+        System.out.println("Int Value "+i);
+        System.out.println("Long Value "+l);
+        System.out.println("Float Value "+f);
+    }
+}
